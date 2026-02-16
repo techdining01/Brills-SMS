372c829693e3c4f2a1d12290293e91ad0d807f15
techdining01 <amooade12@gmail.com>
2026-02-16 17:33:42 +0100

    git commit -m "Ensure Django apps are loaded

--- school_sms/asgi.py
+++ school_sms/asgi.py
@@ -6,20 +6,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_sms.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.urls import path, re_path
-from dashboards.routing import websocket_urlpatterns
-
-# Ensure this import path is correct for your consumer
-# from sms.consumers import PickupCodeConsumer 
 
 django_asgi_app = get_asgi_application()
 
+from dashboards.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(), 
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns, 
-       )
+            websocket_urlpatterns,
+        )
     ),
 })
 
